4662097777b7a9f5beafb5e9f35ec04d4292e4a3
Jensen Omega <omega90ej@gmail.com>
2023-12-09 00:28:48 -0800

    still figuring out p5

--- problems/p5.ts
+++ problems/p5.ts
@@ -5,16 +5,22 @@ import { StarRating } from "@prisma/client";
 // hint:find all stars with the movies "included" on, then good ol' javascript should finish the job
 // This one should require more javascript work than the previous ones
 export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
+   // groupBy groups them together
+   // sumby is getting the total amount
+   // all of this is on remeda
+
+
 
    const allMoviesAvg = []
    // youre using n:number from the paramter as an if statement of the your total avg scores
    // if avg scores > n, show the movies
 
-   // first get movieID,
-   // then get the score for movieID 
-      // if movieID, filter out the score and then reduce
-      // then divide the array by length
-   
+   // // *first get movieID,
+   // // *then get the score for movieID 
+   // *if movieID, filter out the score and then reduce
+      // *for each id === movieID, find the score then reduce
+      // *then divide the array by length
+   // *then if movieId's avg score is over a certain threshold "n" then push the id to an array and return
 
    //this grabs all the object arrays where there is a movie
    const allMovies = await prisma.starRating.findMany({
@@ -22,14 +28,28 @@ export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
          movie: true,
       }
    }) 
-   console.log(allMovies, 'allmovies')
 
 
-   // this shows all the movies with their ID ONLY
    const filterMovieID = allMovies.map((item) => item.movieId);
    // console.log(filterMovieID, 'movieID')
+   const filterMovieScore = allMovies.map((item) => item.score);
 
 
+   const totalScore = 0;
+
+   allMovies.forEach((item) => {
+      const movieID = item.movieId;
+      const movieScore = item.score;
+   })
+
+   // console.log(allMovies, 'allmovies')
+   // this shows all the movies with their ID ONLY
+
+
+   // console.log(filterMovieScore, 'filtermovu')
+   // const moviesWithAvgScore = allMovies.map((item) => {
+   //    if(item.movieId )
+   // })
    
    // const filterMovieID = Object.entries(allMovies.filter(item => item.movieId));
    // console.log(allMovies, 'all movies');
