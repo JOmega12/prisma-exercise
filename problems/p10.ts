53c8a51f4374f794cf9ae6016e167b071fc5079c
Jensen Omega <omega90ej@gmail.com>
2023-12-19 03:07:47 -0800

    finished e11 still working e10

--- problems/p10.ts
+++ problems/p10.ts
@@ -1,4 +1,18 @@
 import { prisma } from "./prisma";
 
 // Deleting a thing, only works swell, if things that reference it are deleted as well
-export const deleteAllUsersWithAgeUnderN = async (n: number) => {};
+export const deleteAllUsersWithAgeUnderN = async (n: number) => {
+
+   const userAge = await prisma.user.findMany({
+      where: {
+         age: {
+            lt: n
+         }
+      }
+   })
+
+   const values = Object.values(userAge);
+   console.log(values, 'values ')
+
+
+};
--- problems/p9.ts
+++ problems/p9.ts
@@ -1,38 +1,11 @@
-import { groupBy, map } from "remeda";
 import { prisma } from "./prisma";
 
 export const updateUsername = async (userId: number, newUsername: string) => {
 
-   const allMovies = await prisma.starRating.findMany({
-      include: {
-         user: true,
+   return await prisma.user.update({
+      where: {id: userId},
+      data: {
+         username: newUsername
       }
-   });
-   // console.log(allMovies, 'allMovies')
-
-   const groupByUserId = groupBy(allMovies ,(user) =>user.userId);
-   
-   const values = Object.values(groupByUserId);
-
-   // return map(values ,(items) => {
-   //    // console.log(items);
-   //    return items.map((ele) => {
-   //       if(ele.userId === userId) {
-   //          ele.user.username = newUsername
-   //       }
-   //    })
-   // })
-
-   // for(const items of values) {
-   //    for(const ele of items) {
-   //       if(ele.userId === userId) {
-   //          ele.user.username = newUsername
-   //          break;
-   //       }
-   //    }
-   // }
-
-   values.forEach((item) => {
-      console.log(item, 'item')
    })
 };
