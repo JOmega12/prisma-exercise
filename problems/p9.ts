0cfafbc96bcd6cade779d4b09d09e1028a13736b
Jensen Omega <omega90ej@gmail.com>
2023-12-18 21:25:42 -0800

    still testing p9

--- problems/p9.ts
+++ problems/p9.ts
@@ -1,8 +1,38 @@
+import { groupBy, map } from "remeda";
 import { prisma } from "./prisma";
 
 export const updateUsername = async (userId: number, newUsername: string) => {
 
-   const allMovies = await prisma.user;
-   console.log(allMovies, 'allMovies')
+   const allMovies = await prisma.starRating.findMany({
+      include: {
+         user: true,
+      }
+   });
+   // console.log(allMovies, 'allMovies')
 
+   const groupByUserId = groupBy(allMovies ,(user) =>user.userId);
+   
+   const values = Object.values(groupByUserId);
+
+   // return map(values ,(items) => {
+   //    // console.log(items);
+   //    return items.map((ele) => {
+   //       if(ele.userId === userId) {
+   //          ele.user.username = newUsername
+   //       }
+   //    })
+   // })
+
+   // for(const items of values) {
+   //    for(const ele of items) {
+   //       if(ele.userId === userId) {
+   //          ele.user.username = newUsername
+   //          break;
+   //       }
+   //    }
+   // }
+
+   values.forEach((item) => {
+      console.log(item, 'item')
+   })
 };
