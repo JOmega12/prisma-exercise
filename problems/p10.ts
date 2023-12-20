b93503604f579ddc479f46f9f6e29d6500ef6ae7
Jensen Omega <omega90ej@gmail.com>
2023-12-19 20:45:57 -0800

    still working on p10

--- problems/p10.ts
+++ problems/p10.ts
@@ -3,16 +3,62 @@ import { prisma } from "./prisma";
 // Deleting a thing, only works swell, if things that reference it are deleted as well
 export const deleteAllUsersWithAgeUnderN = async (n: number) => {
 
-   const userAge = await prisma.user.findMany({
+   // const users = await prisma.user.update({
+   //    select: {
+   //       age: true
+   //    },
+   //    data: {
+   //       age: {
+   //          gt: n
+   //       }
+   //    }
+   //    // data: {
+   //    //    age: {
+   //    //       deleteMany: {
+   //    //          lt: n
+   //    //       }
+   //    //    }
+   //    // }
+   // })
+
+   // return users;
+
+
+   const users = await prisma.user.findMany({
       where: {
          age: {
             lt: n
          }
       }
    })
+   console.log(users, 'users')
 
-   const values = Object.values(userAge);
-   console.log(values, 'values ')
+   const userIds = users.map((user) => user.id);
+   const deleteUsers = await prisma.user.deleteMany({
+      where:{
+         id: {
+            in: userIds
+         }
+      }
+   })
+   return deleteUsers;
+
+   // const deleteUser = await prisma.user.delete({
+   //    where:{
+   //       id: 4934
+   //    }
+   // })
+
+   // console.log(deleteUser, ' deleteUser')
+   // return deleteUser;
+
+   // return await prisma.user.deleteMany({
+   //    where: {
+   //       age: {
+   //          lt: n
+   //       }
+   //    }
+   // })
 
 
 };
