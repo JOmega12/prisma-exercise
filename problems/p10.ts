0e9f151600f949eb3bdbfa545851c2ec1997dccb
Jensen Omega <omega90ej@gmail.com>
2023-12-21 01:58:08 -0800

    still working on p10

--- problems/p10.ts
+++ problems/p10.ts
@@ -24,19 +24,24 @@ export const deleteAllUsersWithAgeUnderN = async (n: number) => {
    // return users;
 
 
-   const users = await prisma.user.findMany()
+   const users = await prisma.user.findMany({
+      where: {
+         age: {
+            lt: n
+         }
+      }
+   })
 
    // console.log(users, 'users')
 
    const values = Object.values(users);
    console.log(values, 'val')
+   const itemsLessThanN = values
+   .filter((item) => item.age < n ? item: null);
+   console.log(itemsLessThanN, 'itemsLessThanN')
 
-   // ? I think the test is asking my to update the array from prisma.user and delete all 
-   const itemsGreaterThanN = values
-   .filter((item) => item.age > n);
-   // .filter((item) => item.age > n);
-   console.log(itemsGreaterThanN, 'item')
-   
+
+
    // const deleteUsersOver20 =  users.map((user) => {
    //    const userOver20 = prisma.user.delete({
    //       where: {
@@ -49,10 +54,5 @@ export const deleteAllUsersWithAgeUnderN = async (n: number) => {
    //    console.log(userOver20, 'user>20')
    // })
 
-
-   // const values = Object.values(users);
-   // console.log(values, 'val')
-   // .filter((item) => item.age > 20);
-   // console.log(findUsersUnder20, 'over20')
 
 };
