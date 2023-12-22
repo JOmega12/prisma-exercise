e9d9d5cc22accbb3122d15a545fbc9db9f093def
Jensen Omega <omega90ej@gmail.com>
2023-12-22 02:23:01 -0800

    trying to rework p5

--- problems/p5.ts
+++ problems/p5.ts
@@ -5,20 +5,6 @@ import { StarRating } from "@prisma/client";
 // hint:find all stars with the movies "included" on, then good ol' javascript should finish the job
 // This one should require more javascript work than the previous ones
 export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
-   // groupBy groups them together
-   // sumby is getting the total amount
-   // all of this is on remeda
-
-   // youre using n:number from the paramter as an if statement of the your total avg scores
-   // if avg scores > n, show the movies
-
-   // // *first get movieID,
-   // // *then get the score for movieID 
-   // *if movieID, filter out the score and then reduce
-      // *for each id === movieID, find the score then reduce
-      // *then divide the array by length
-   // *then if movieId's avg score is over a certain threshold "n" then push the id to an array and return
-
    //this grabs all the object arrays where there is a movie
    const allMovies = await prisma.starRating.findMany({
       include: {
@@ -42,9 +28,7 @@ export const getAllMoviesWithAverageScoreOverN = async (n: number) => {
    [] as number[]
    );
    const sortedMovies = sortBy(moviesWithAvgScore, (movie) => movie)
-   // console.log(sortedMovies)
    return sortedMovies
-   // return moviesWithAvgScore
 
 };
 
