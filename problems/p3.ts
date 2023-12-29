e9f5ff4314cee831bcd621afc0d2d821bfe4f70e
Jensen Omega <omega90ej@gmail.com>
2023-12-29 01:25:37 -0800

    fixed p3

--- problems/p3.ts
+++ problems/p3.ts
@@ -4,14 +4,11 @@ import { prisma } from "./prisma";
 // hint: the hot tub is hot, the water is great, to solve this problem you should "aggregate"
 export const getAverageUserAge = async () => {
 
-   const userAge = await prisma.user.findMany({
-      select: {
-         age: true
+   const userAge = await prisma.user.aggregate({
+      _avg: {
+         age: true,
       }
    })
 
-   const totalAge = userAge.reduce((total, user) => total + user.age, 0);
-
-   const avgAge = totalAge / userAge.length;
-   return avgAge;
+   return userAge._avg.age;
 };
